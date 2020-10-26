b2411dd5f257d3d3b99a86e670417c7b8c195468
Jhonatan C Hurtado <jhonatanchurtado@gmail.com>
2020-10-26 12:05:26 -0500

    Se agregó y ajusto la base de datos

--- empleados.sql
+++ empleados.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-10-2020 a las 00:22:21
+-- Tiempo de generación: 26-10-2020 a las 18:04:56
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.10
 
@@ -39,8 +39,10 @@ CREATE TABLE `areas` (
 --
 
 INSERT INTO `areas` (`id`, `nombre`, `created_at`, `updated_at`) VALUES
-(1, 'Gráfico', NULL, NULL),
-(2, 'Desarrollo', NULL, NULL);
+(1, 'Desarrollo', NULL, NULL),
+(2, 'Diseño', NULL, NULL),
+(3, 'Comunicación', NULL, NULL),
+(4, 'Video', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -55,6 +57,13 @@ CREATE TABLE `empleado_rol` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `empleado_rol`
+--
+
+INSERT INTO `empleado_rol` (`id`, `user_id`, `rol_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 3, '2020-10-26 21:57:55', '2020-10-26 21:57:55');
 
 -- --------------------------------------------------------
 
@@ -73,11 +82,11 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_10_154709_create_areas_table', 1),
-(2, '2014_10_11_154846_create_roles_table', 1),
-(3, '2014_10_12_000000_create_users_table', 1),
-(4, '2014_10_12_100000_create_password_resets_table', 1),
-(5, '2020_10_24_155122_create_empleado_rol_table', 1);
+(11, '2014_10_10_154709_create_areas_table', 1),
+(12, '2014_10_11_154846_create_roles_table', 1),
+(13, '2014_10_12_000000_create_users_table', 1),
+(14, '2014_10_12_100000_create_password_resets_table', 1),
+(15, '2020_10_24_155122_create_empleado_rol_table', 1);
 
 -- --------------------------------------------------------
 
@@ -124,12 +133,20 @@ CREATE TABLE `users` (
   `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `sexo` char(25) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `boletin` char(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `boletin` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `area_id` int(10) UNSIGNED NOT NULL,
+  `descripcion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `nombre`, `email`, `sexo`, `boletin`, `area_id`, `descripcion`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Jhonatan', 'jhonatanchurtado@gmail.com', 'masculino', 'No', 1, 'Bio', NULL, '2020-10-26 21:57:55', '2020-10-26 21:57:55');
 
 --
 -- Índices para tablas volcadas
@@ -183,31 +200,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `areas`
 --
 ALTER TABLE `areas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `empleado_rol`
 --
 ALTER TABLE `empleado_rol`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
